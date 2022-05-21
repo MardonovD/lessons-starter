d52c8a141624e774f35ab1c9ff313053ea5cb366
Dostonbek <mardonovdoston69@gmail.com>
2022-05-21 15:05:59 +0300

    dmv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import Header from "./containers/Header";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-} from "react-router-dom";
+import Header from "./containers/Header.js";
 
-import Main from "./containers/Main";
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Header />
-        </Route>
-        <Route path="/main">
-          <Main />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,19 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="ui pointing menu">
-      <div className="ui container">
-        <a className="active item">Home</a>
-        <a className="item">Messages</a>
-        <a className="item">Friends</a>
-        <div className="right menu">
-          <div className="item">
-            <div className="ui transparent icon input">
-              <input type="text" placeholder="Search..." />
-              <i className="search link icon" />
-            </div>
-          </div>
-        </div>
+    <div className="ui fixed menu">
+      <div className="ui container center ">
+        <h2>DosShop </h2>
       </div>
     </div>
   );
